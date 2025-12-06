97f2dec44b29db9fc9892a1e851cf30d563895b0
Guilh3rme-M3ndes <guilheu2004@gmail.com>
2025-12-06 01:42:58 -0300

    'added destinations, vehicles and garages submit'

--- cs-fretamento/Aeroporto.cs
+++ cs-fretamento/Aeroporto.cs
@@ -20,7 +20,8 @@ namespace cs_fretamento
         {
             this.id = id;
             this.nome = nome;
-            garagens = new List<Garagem>();
+            Garagem defaultGaragem = new Garagem($"{Nome} 1", 10);
+            garagens = new List<Garagem>([defaultGaragem]);
         }
 
         public void AdicionarGaragem(Garagem garagem)
